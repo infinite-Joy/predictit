78660c8ca83be3fe7e882f3b043206970917c92a
hhsecond <sherinct@live.com>
2016-06-07 12:28:08 +0530

    modified script to save the source

--- project_cricbuz/project_cricbuz/spiders/cricbuz_spider.py
+++ project_cricbuz/project_cricbuz/spiders/cricbuz_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+import os #for creating directory
 pref = ''
 
 class cricbuz_spider(scrapy.Spider):
@@ -15,25 +16,48 @@ class cricbuz_spider(scrapy.Spider):
 		pref = '/'.join(response.url.split('/')[:-1])
 		for year in years:
 			time.sleep(5)
-			yield scrapy.Request((pref + year), callback = self.series_parser)
+			num_year = year.split('/')[-1]
+			request = scrapy.Request((pref + year), callback = self.series_parser)
+			request.meta['num_year'] = num_year
+			yield request
 
 	def series_parser(self, response):
 		global pref
 		series = response.xpath('//div[@class="cb-col-16 cb-col text-bold cb-srs-cat" and ./text()="International"]/following-sibling::div[1]/div/a/@href').extract()
 		for each_series in series:
 			time.sleep(5)
-			yield scrapy.Request((pref + each_series), callback = self.match_parser)
+			str_each_series = each_series.split('/')[-2]
+			request = scrapy.Request((pref + each_series), callback = self.match_parser)
+			request.meta['num_year'] = response.meta['num_year']
+			request.meta['str_each_series'] = str_each_series
+			yield request
 
 	def match_parser(self, response):
 		global pref
 		matches = response.xpath('//div[@class="cb-col-60 cb-col cb-srs-mtchs-tm"]/a[1]/@href').extract()
 		for match in matches:
-			#time.sleep(5)
-			print(match)
 			time.sleep(5)
-			yield scrapy.Request((pref + match), callback = self.match_details)
+			request = scrapy.Request((pref + match), callback = self.match_details)
+			request.meta['num_year'] = response.meta['num_year']
+			request.meta['str_each_series'] = response.meta['str_each_series']
+			request.meta['match_name'] = match.split('/')[-1]
+			yield request
 
 	def match_details(self, response):
-		print(response.body)
+		r = response.meta
+		url = response.url
+		default_val = 'cricket-scores' #default value in the url which we need to replace for getting different pages
+		path = str(r['num_year']) + '/' + str(r['str_each_series']) + '/' + str(r['match_name']) + '/'
+		for page in ['cricket-scores','cricket-scorecard','live-cricket-match-blog','cricket-match-facts']:
+			current_url = url.replace(default_val, page)
+			current_path = path + page + '.html'
+			time.sleep(5)
+			request = scrapy.Request(current_url, callback = self.writefile)
+			request.meta['current_path'] = current_path
+			yield request
 
-
+	def writefile(self, response):
+		current_path = response.meta['current_path']
+		os.makedirs(os.path.dirname(current_path), exist_ok = True)
+		with open('current_path', 'w+') as f:
+				f.write(response.body)
