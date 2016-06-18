d7e0d36e61e8024839ff5ff03a6953e8280b60f9
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2016-06-18 09:20:08 +0000

    cleaning

---
+++ project_cricbuz/generate_data.py
@@ -0,0 +1,17 @@
+
+from parse_html import invalid_match_stat
+
+def get_Xs():
+    """
+    define the X axis
+    """
+    pass
+
+def get_Y():
+    """
+    define the Y axis
+    """
+    pass
+
+def define_the_model():
+    pass
